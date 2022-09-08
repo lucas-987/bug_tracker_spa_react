b5af38705754d4046a62555618962e78d2262b11
lucas-987 <69002344+lucas-987@users.noreply.github.com>
2022-09-08 14:52:40 +0200

    #11: Form validation done.

--- src/components/ProjectPage/AddBugAlert.tsx
+++ src/components/ProjectPage/AddBugAlert.tsx
@@ -12,6 +12,21 @@ const PRIORITY_PLACEHOLDER = PRIORITY_LABEL
 const DUE_DATE_LABEL = "Due date (optionnal)"
 const DUE_DATE_PLACEHOLDER = "Due date"
 
+const FIELD_REQUIRED = "This field is required."
+
+const TITLE_TOO_LONG = ["The title must be less than ", " characters (currently ", ")."]
+const DESCRIPTION_TOO_LONG = ["The description must be less than ", " characters (currently ", ")."]
+const MAX_TITLE_LENGTH = 255
+const MAX_DESCRIPTION_LENGTH = 65535
+
+function fieldToLong(strings: string[], maxChar: number, currentLength: number) {
+    const str0 = strings[0] // ... must be less than
+    const str1 = strings[1] // ... characters ... (currently 
+    const str2 = strings[2] // ).
+
+    return `${str0}${maxChar}${str1}${currentLength}${str2}`
+}
+
 interface Props {
     onClick?: () => void;
     onClose: () => void;
@@ -25,11 +40,32 @@ function AddBugAlert({ onClick, onClose, onBugCreated, projectId }: Props) {
     const [priority, setPriority] = useState<number>(1)
     const [dueDate, setDueDate] = useState<Date>()
 
+    const [titleError, setTitleError] = useState<string>('')
+    const [descriptionError, setDescriptionError] = useState<string>('')
+
     const createNewBug = (e: React.SyntheticEvent) => {
         e.preventDefault()
 
-        //check if the fields are ok
+        let formIsValid = true
 
+        //check if the fields are ok
+        if(!title) {
+            setTitleError(FIELD_REQUIRED);
+            formIsValid = false;
+        }
+        else if(title.length > MAX_TITLE_LENGTH) {
+            setTitleError(fieldToLong(TITLE_TOO_LONG, MAX_TITLE_LENGTH, title.length));
+            formIsValid = false;
+        }
+        else setTitleError("")
+
+        if(description.length > MAX_DESCRIPTION_LENGTH) {
+            setDescriptionError(fieldToLong(DESCRIPTION_TOO_LONG, MAX_DESCRIPTION_LENGTH, description.length));
+            formIsValid = false;
+        }
+        else setDescriptionError("")
+
+        if(!formIsValid) return;
 
         let newBug:Bug = {
             id: -1,
@@ -64,23 +100,29 @@ function AddBugAlert({ onClick, onClose, onBugCreated, projectId }: Props) {
             <div className="alert">
                 <h1>{FORM_TITLE}</h1>
                 <form className="form" onSubmit={createNewBug}>
-                    <div className="form-group">
+                    <div className={"form-group" + (titleError != "" ? " error" : "")}>
                         <label htmlFor="title">{TITLE_LABEL}</label>
                         <input name="title" type="text" placeholder={TITLE_PLACEHOLDER}
                             value={title} onChange={(e) => setTitle(e.target.value)} />
+                        
+                        <span className="error-message">{titleError}</span>
                     </div>
                     
-                    <div className="form-group">
+                    <div className={"form-group" + (descriptionError != "" ? " error" : "")}>
                         <label htmlFor="description">{DESCRIPTION_LABEL}</label>
                         <textarea name="description" placeholder={DESCRIPTION_PLACEHOLDER}
                             rows={10} 
                             value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
+                        
+                        <span className="error-message">{descriptionError}</span>
                     </div>
 
-                    <div className="form-group">
+                    <div className={"form-group"/* + (priorityError != "" ? " error" : "")*/}>
                         <label htmlFor="priority">{PRIORITY_LABEL}</label>
                         <input name="priority" type="number" placeholder={PRIORITY_PLACEHOLDER} min="0"
                             value={priority} onChange={(e) => setPriority(Number(e.target.value))}/>
+                        
+                        <span className="error-message">{/*priorityError*/}</span>
                     </div>
 
                     <div className="form-group">
--- src/components/ProjectsPage/CreateProject.tsx
+++ src/components/ProjectsPage/CreateProject.tsx
@@ -7,6 +7,22 @@ const DESCRIPTION_LABEL = "Description"
 const DESCRIPTION_PLACEHOLDER = "Description (optionnal) ..."
 const CREATE_BUTTON_TEXT = "Create"
 
+const FIELD_REQUIRED = "This field is required."
+
+const TITLE_TOO_LONG = ["The title must be less than ", " characters (currently ", ")."]
+const DESCRIPTION_TOO_LONG = ["The description must be less than ", " characters (currently ", ")."]
+const MAX_TITLE_LENGTH = 255
+const MAX_DESCRIPTION_LENGTH = 65535
+
+function fieldToLong(strings: string[], maxChar: number, currentLength: number) {
+    const str0 = strings[0] // ... must be less than
+    const str1 = strings[1] // ... characters ... (currently 
+    const str2 = strings[2] // ).
+
+    return `${str0}${maxChar}${str1}${currentLength}${str2}`
+}
+
+
 interface Props {
     onProjectCreated: (newProject: Project) => void
 }
@@ -15,19 +31,32 @@ function CreateProject({ onProjectCreated }: Props) {
     const [isOpen, setIsOpen] = useState<boolean>(false);
     const [title, setTitle] = useState<string>("");
     const [description, setDescription] = useState<string>("");
+    const [titleError, setTitleError] = useState<string>("");
+    const [descriptionError, setDescriptionError] = useState<string>("");
 
     const createNewProject = (e: React.SyntheticEvent) => {
         e.preventDefault();
+        let formIsValid = true;
 
         //check if fields are ok
-        if(!title || title.length > 255) {
-            return;
+        if(!title) {
+            setTitleError(FIELD_REQUIRED);
+            formIsValid = false;
         }
+        else if(title.length > MAX_TITLE_LENGTH) {
+            setTitleError(fieldToLong(TITLE_TOO_LONG, MAX_TITLE_LENGTH, title.length));
+            formIsValid = false;
+        }
+        else setTitleError("")
 
-        if(description.length > 65535) {
-            return;
+        if(description.length > MAX_DESCRIPTION_LENGTH) {
+            setDescriptionError(fieldToLong(DESCRIPTION_TOO_LONG, MAX_DESCRIPTION_LENGTH, description.length));
+            formIsValid = false;
         }
+        else setDescriptionError("")
         
+        if(!formIsValid) return;
+
         let newProject: Project = {
             id: -1,
             title,
@@ -45,16 +74,19 @@ function CreateProject({ onProjectCreated }: Props) {
         return (
             <div className="project-add-form">
                 <form className="form--full-width" onSubmit={createNewProject}>
-                    <div className="form-group">
+                    <div className={"form-group" + (titleError != "" ? " error" : "")}>
                         <label htmlFor="title">{TITLE_LABEL}</label>
                         <input name="title" type="text" value={title} placeholder={TITLE_PLACEHOLDER}
                             onChange={(e) => setTitle(e.target.value)} />
+                        <span className="error-message">{titleError}</span>
                     </div>
                         
-                    <div className="form-group">
+                    <div className={"form-group" + (descriptionError != "" ? " error" : "")}>
                         <label htmlFor="description">{DESCRIPTION_LABEL}</label>
                         <textarea name="description" value={description} placeholder={DESCRIPTION_PLACEHOLDER}
                             rows={10} onChange={(e) => setDescription(e.target.value)} />
+                        
+                        <span className="error-message">{descriptionError}</span>
                     </div>
                         
                     <button>{CREATE_BUTTON_TEXT}</button>
